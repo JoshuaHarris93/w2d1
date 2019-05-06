ce663db1c506d0fe4da60cdab43dfe5e1c63f6d9
JoshuaHarris93 <josh@emptychairproductions.com>
2019-05-06 18:26:37 +0000

    each chunk is now separated

--- getPrintHTML.js
+++ getPrintHTML.js
@@ -1,40 +1,62 @@
+// while https is built-in to Node, it is a module, so it must be required
+var https = require('https');                               
 
-var https = require('https');  
+// the host can be thought of as the domain name you want to read from,
+// and the path is the resource - '/' is the root path, but if you wanted to read a
+// particular resource (like '/login/index.html'), that would be defined in the path
 
-  function getAndPrintHTML () {
+
+
+function getAndPrintHTML () {
 
     var requestOptions = {
-      host: 'sytantris.github.io',
-      path: '/http-examples/step2.html'
-    };
-    var sum = function sum(data) {
-        return arr.map((a, b) => a + b)
-      }
-    var eachChunk = data.
-        console.log(add);
-    }
+        host: 'sytantris.github.io',
+        path: '/http-examples/step1.html'
+      };
+
+    //   var sum = function sum() {
+    //     var totalSum;
+    //     if (arr.length === 0) {
+    //       return 0;
+    //     } else {
+    //       totalSum = arr.reduce(function(a, b) {
+    //         return a + b;
+    //       });
+    //     }
+    //     return totalSum;
+
+    //   }
+
+    
+    var eachChunk = '';
+      
 
     https.get(requestOptions, function (response) {
-
+        
         // set encoding of received data to UTF-8
         response.setEncoding('utf8');
       
         // the callback is invoked when a `data` chunk is received
         response.on('data', function (data) {
-          console.log('Chunk Received. Length:', data.length  + '\n');
+            
+            eachChunk += data; 
+          
         });
-
-       
       
         // the callback is invoked when all of the data has been received
         // (the `end` of the stream)
         response.on('end', function() {
-          console.log('Response stream complete.');
+            console.log('Chunk Received. Length:', eachChunk  + '\n');
+            console.log('Response stream complete.');
+          
         });
       
       });
-  
-  
+    
   }
 
-  getAndPrintHTML ();
+// notice that https.get takes a callback with one parameter -
+// response, which is a Stream that represents the HTTP response
+
+
+getAndPrintHTML();
