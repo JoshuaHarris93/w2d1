13a84799eb52cf8c2aa56d2203323b30005af521
JoshuaHarris93 <josh@emptychairproductions.com>
2019-05-06 18:32:27 +0000

    first try parameters

---
+++ WithParameters.js
@@ -0,0 +1,41 @@
+//Printing with Parameters
+
+var https = require('https');                               
+
+var options = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+function getAndPrintHTML (options) {
+
+  
+
+    
+    var eachChunk = '';
+      
+
+    https.get(requestOptions, function (response) {
+        
+        // set encoding of received data to UTF-8
+        response.setEncoding('utf8');
+      
+        // the callback is invoked when a `data` chunk is received
+        response.on('data', function (data) {
+            
+            eachChunk += data; 
+          
+        });
+      
+        response.on('end', function() {
+            console.log('Chunk Received. Length:', eachChunk  + '\n');
+            console.log('Response stream complete.');
+          
+        });
+      
+      });
+    
+  }
+
+
+getAndPrintHTML(options);
